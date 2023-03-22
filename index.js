d3201fc44cd80b6e5b12a0cac6a76a91ba9ffcd4
Hajra <hajria0158@gmail.com>
2023-03-22 20:29:28 +0500

    Assignment 1

---
+++ index.js
@@ -0,0 +1,163 @@
+// 1.Environment setup
+//vs code is an text editor and IDE integerated developement environment
+//vs code is general hybird of th
+
+// download vs code
+
+//  strongly typed languages statically typed languages
+
+// java and c++
+// int a=23;
+//there are two things Explicit and implicit
+
+// explicit
+
+// like java and c++
+//explicit you tell by your own
+// int myvar=42;
+//string mystring= "i am a string"; any thing in single or double quots its called string
+
+// mystrinv = 42;its not allowed in static typed language or strongly typed languages
+
+// implicit
+
+//  javascript php python
+
+// javascript understand on its own that it has certain type of values dont have to tell javascript
+
+//  var myvar= 42;
+
+// myvar = "i am a string" in javascript allowed in dynmically typed languages (javascript is dynamic language)
+
+//2. datatypes
+
+// there are 7 type of datatype in javascript
+
+//Numbers
+
+// -2 -1 0 1 2 80 are called Integers
+
+// -1.1 2.0 3.8 these are all floating point datatypes
+
+// these both are number
+
+// string
+
+// any string inside  single or double quotes are called string
+
+// 3.Boolean
+
+// true or 1
+
+// false or 0
+
+// condition statements are boolean
+
+// gives value means you define it like var userislogin = true; its defined
+
+// 4.Null
+
+// there is nothing , void or empty
+
+// var nullexmaple=null;
+
+// 5.undefined
+
+// undefined means variable is declare but not define
+
+// var myvar; its only declare not defined it will implicitly add undefined to this variables
+
+// undefined = undefined; you can explicitly defined it but dont need to define because javascript will define it on.
+
+// 6.objects are king in javscript everything is object is object in javascript
+
+// if you understand object you understand javascript
+
+//7.Arrays
+
+//
+//
+//
+//
+//
+//8.Variables
+
+//variable is very important
+
+//variable is like a container
+
+// how variable are defined
+
+// in 3 ways we can define variables
+
+// var , let and const
+
+// let and const are part or Ecmascript 6/ Ecmascript 2015
+
+// ecmascript is basically in javascript
+
+// we will not discuss let and const in this lecture
+
+// we will use var
+
+// var myName =" Hajra";
+
+// rules to set variables
+
+// defference b/t convension and rules
+
+// if you dont follow rules then javascript will throw errors
+
+// convension it is not necesary to follow but if you follow it called as good practice
+
+// Rules
+
+// 1. variable name  never be started with numbers
+
+// dont
+
+// var 1name you cantstart from numbers
+
+//dos
+
+//var name1;
+
+//var javascript;
+
+//var first _name;
+
+//convension
+
+//good practice using variable name in camel case
+//
+//
+//
+//
+//
+var myVar = "Hajra";
+console.log(myVar);
+console.log(myVar);
+
+//case sensitive
+
+//java c++ javascript etc
+
+// var myname = "HAJRA"
+
+// console.log(myname)
+
+// console.log(myName) this is wrong
+
+//case insensitive
+
+// php code example etc
+
+//  $myvar ="Hajra";
+
+//print($myvar);
+
+//print($myVar);
+
+//print($Myvar);
+
+//these are all valid in php because it is case insensitive language
