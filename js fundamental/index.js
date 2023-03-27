4ed43870cc75601f0ee21403f24241c95d08a197
Hajra <hajria0158@gmail.com>
2023-03-27 13:32:38 +0500

    assignment 1

---
+++ js fundamental/index.js
@@ -0,0 +1,18 @@
+// syntax error
+// var 1name="Hajra";
+// console.log(1name);
+
+var name = "Hajra";
+console.log(name);
+
+// logical error
+function multiplication(val1, val2) {
+  console.log(val1 + val2);
+}
+multiplication(2, 5);
+
+// runtime error
+console.log("hello");
+// console.log(asa);runtime error
+console.log("hello");
+console.log("hello");
